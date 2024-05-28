4b6420039a2e7913065dbc7879430fc3271137a9
Leyder Quintero <desarrollador9@wedevel.co>
2024-05-27 20:03:36 -0500

    inicializar el servidor

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('jeje')
+import express, { Express, Request, Response } from "express";
+import dotenvFlow from "dotenv-flow";
+dotenvFlow.config()
+
+const PORT = process.env['PORT'];
+
+
+const app: Express = express();
+
+//* Test if the server is running
+app.get('/', (req: Request, res: Response) => {
+    res.send('Express server Initialized');
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+});
